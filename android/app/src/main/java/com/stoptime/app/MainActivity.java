8781556c52f568f00697b9d03b5284ec47ad9a47
StopTime <deploy@stoptime.app>
2026-07-08 16:49:22 -0400

    Agregando carpeta de Android nativa (Capacitor)

---
+++ android/app/src/main/java/com/stoptime/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.stoptime.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.stoptime.app',
+  appName: 'Stop Time Sushi',
+  webDir: 'dist'
+};
+
+export default config;
